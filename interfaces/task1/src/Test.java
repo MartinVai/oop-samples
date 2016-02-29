4b284316e9a8f1958fe81d3807b2143f82d0b2be
Märt Bakhoff <mart.bakhoff@gmail.com>
2016-02-29 16:31:16 +0200

    array init

--- interfaces/task1/src/Test.java
+++ interfaces/task1/src/Test.java
@@ -1,11 +1,7 @@
 public class Test {
   public static void main(String[] args) {
-    IdKaart[] idKaardid = new IdKaart[]{
-        new IdKaart("39108071234")
-    };
-    Õpilaspilet[] õpilaspiletid = new Õpilaspilet[]{
-        new Õpilaspilet(2001)
-    };
+    IdKaart[] idKaardid = { new IdKaart("39108071234") };
+    Õpilaspilet[] õpilaspiletid = { new Õpilaspilet(2001) };
 
     AmeerikaMäed ameerikaMäed = new AmeerikaMäed();
     System.out.println(ameerikaMäed.vanusedSobivad(idKaardid, õpilaspiletid));
