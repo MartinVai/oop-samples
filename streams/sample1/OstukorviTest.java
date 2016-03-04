5d26481a0fd5f9897f88ae4be7fc5ed12582836a
Märt Bakhoff <mart.bakhoff@gmail.com>
2016-03-05 00:20:24 +0200

    rename class

--- streams/sample1/OstukorviTest.java
+++ streams/sample1/OstukorviTest.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.Arrays;
 
-public class Test {
+public class OstukorviTest {
   public static void main(String[] args) throws Exception {
     Toode shokolaad = new Toode("shokolaad", 1.0, 1.29);
     Toode apelsin = new Toode("apelsin", 2.5, 0.89);
